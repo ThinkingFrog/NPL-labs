6b1466832c63c47640be2b654d2ff5d09506dd48
ThinkingFrog <khripunkovdv@gmail.com>
2023-11-21 23:16:29 +0300

    Use Arc to remove array copies

--- parallel_array_sum_rust/src/main.rs
+++ parallel_array_sum_rust/src/main.rs
@@ -1,15 +1,18 @@
+use std::sync::Arc;
 use std::thread;
 
 fn main() {
-    let arr: Vec<u64> = (0..=1e8 as u64).collect();
+    let arr: Arc<Vec<u64>> = Arc::new((0..=1e8 as u64).collect());
     let chunk_size = 1e5 as usize;
 
     let thread_count = arr.len() / chunk_size;
     let mut thread_handles: Vec<thread::JoinHandle<u64>> = vec![];
 
     for idx in 0..thread_count {
-        let chunk = arr[idx * chunk_size..chunk_size * idx + 1].to_vec();
-        thread_handles.push(thread::spawn(move || chunk.iter().sum()));
+        let arr_pt = Arc::clone(&arr);
+        thread_handles.push(thread::spawn(move || {
+            arr_pt[idx * chunk_size..chunk_size * idx + 1].iter().sum()
+        }));
     }
 
     let mut all_sum = 0;
