fb2d42223efdab37e2b132c2df0866557b1a0dff
ThinkingFrog <khripunkovdv@gmail.com>
2023-11-30 14:30:49 +0300

    Implement stream reading

--- word_count_erlang/main.erl
+++ word_count_erlang/main.erl
@@ -2,11 +2,29 @@
 -export([count_words/1]).
 
 count_words(FilePath) ->
-    {ok, BinaryData} = file:read_file(FilePath),
-    Text = binary_to_list(BinaryData),
-    Words = count_words_in_text(Text),
-    io:format("Number of words in the file: ~p~n", [Words]).
+    {ok, File} = file:open(FilePath, [read, raw, binary]),
+    WordCount = count_words_in_stream(File, 0),
+    file:close(File),
+    FinalWordCount = if
+        WordCount > 0 -> WordCount + 1;
+        true -> WordCount
+    end,
+    io:format("Number of words in the file: ~p~n", [FinalWordCount]).
 
-count_words_in_text(Text) ->
-    Words = string:tokens(Text, " \t\n"),
-    length(Words).
+count_words_in_stream(File, WordCount) ->
+    case file:read(File, 1) of
+        {ok, <<Char/binary>>} ->
+            NewWordCount = case Char of
+                <<32>> -> % space
+                    WordCount + 1;
+                <<10>> -> % \n
+                    WordCount + 1;
+                <<9>> -> % \t
+                    WordCount + 1;
+                _ ->
+                    WordCount
+            end,
+            count_words_in_stream(File, NewWordCount);
+        eof ->
+            WordCount
+    end.
